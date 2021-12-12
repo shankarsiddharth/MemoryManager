5886751a98e8362b0d479a604708ae54d4baf0f4
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2021-12-12 14:06:44 -0700

    Refactor Memory Alignment Code

---
+++ FinalExam/MemoryAlignmentHelpers.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <cstdint>
+#include <cassert>
+
+class MemoryAlignmentHelpers
+{
+public:
+	static inline size_t AlignSize(size_t i_size, size_t i_align = sDefaultAlignment) {
+		return (i_size + i_align - 1) & ~(i_align - 1);
+	}
+
+	static inline size_t AlignPadding(size_t i_size, size_t i_align = sDefaultAlignment) {
+		return AlignSize(i_size, i_align) - i_size;
+	}
+
+	static inline bool IsPowerOfTwo(size_t i_align)
+	{
+		return (i_align & (i_align - 1)) == 0;
+	}
+
+	static inline uintptr_t AlignDown(uintptr_t i_value, size_t i_align)
+	{
+		assert(i_align);
+		assert(IsPowerOfTwo(i_align));
+		return i_value & ~(i_align - 1);
+	}
+
+	static inline uintptr_t AlignUp(uintptr_t i_value, size_t i_align)
+	{
+		assert(i_align);
+		assert(IsPowerOfTwo(i_align));
+		return (i_value + (i_align - 1)) & ~(i_align - 1);
+	}
+
+	static const size_t sDefaultAlignment = sizeof(size_t);
+};
