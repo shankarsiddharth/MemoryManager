684ddc191ed20516fe0a5512cc42e7292b2039a4
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2021-12-14 10:34:49 -0700

    Rename the Default HeapManager to LinkedListAllocator

--- FinalExam/Allocators.cpp
+++ FinalExam/Allocators.cpp
@@ -3,52 +3,52 @@
 
 #include <stdio.h>
 
-#include "HeapManager.h"
+#include "LinkedListAllocator.h"
 
 void * __cdecl malloc(size_t i_size)
 {
-	// replace with calls to your HeapManager or FixedSizeAllocators
+	// replace with calls to your LinkedListAllocator or FixedSizeAllocators
 	printf("malloc %zu\n", i_size);
 	//return _aligned_malloc(i_size, 4);
-	return HeapManager::Get()->Alloc(i_size);
+	return LinkedListAllocator::Get()->Alloc(i_size);
 }
 
 void __cdecl free(void * i_ptr)
 {
-	// replace with calls to your HeapManager or FixedSizeAllocators
+	// replace with calls to your LinkedListAllocator or FixedSizeAllocators
 	printf("free 0x%" PRIXPTR "\n", reinterpret_cast<uintptr_t>(i_ptr));
 	//return _aligned_free(i_ptr);
-	HeapManager::Get()->Free(i_ptr);
+	LinkedListAllocator::Get()->Free(i_ptr);
 }
 
 void * operator new(size_t i_size)
 {
-	// replace with calls to your HeapManager or FixedSizeAllocators
+	// replace with calls to your LinkedListAllocator or FixedSizeAllocators
 	printf("new %zu\n", i_size);
 	//return _aligned_malloc(i_size, 4);
-	return HeapManager::Get()->Alloc(i_size);
+	return LinkedListAllocator::Get()->Alloc(i_size);
 }
 
 void operator delete(void * i_ptr)
 {
-	// replace with calls to your HeapManager or FixedSizeAllocators
+	// replace with calls to your LinkedListAllocator or FixedSizeAllocators
 	printf("delete 0x%" PRIXPTR "\n", reinterpret_cast<uintptr_t>(i_ptr));
 	//return _aligned_free(i_ptr);
-	HeapManager::Get()->Free(i_ptr);
+	LinkedListAllocator::Get()->Free(i_ptr);
 }
 
 void * operator new[](size_t i_size)
 {
-	// replace with calls to your HeapManager or FixedSizeAllocators
+	// replace with calls to your LinkedListAllocator or FixedSizeAllocators
 	printf("new [] %zu\n", i_size);
 	//return _aligned_malloc(i_size, 4);
-	return HeapManager::Get()->Alloc(i_size);
+	return LinkedListAllocator::Get()->Alloc(i_size);
 }
 
 void operator delete [](void * i_ptr)
 {
-	// replace with calls to your HeapManager or FixedSizeAllocators
+	// replace with calls to your LinkedListAllocator or FixedSizeAllocators
 	printf("delete [] 0x%" PRIXPTR "\n", reinterpret_cast<uintptr_t>(i_ptr));
 	//return _aligned_free(i_ptr);
-	HeapManager::Get()->Free(i_ptr);
+	LinkedListAllocator::Get()->Free(i_ptr);
 }
