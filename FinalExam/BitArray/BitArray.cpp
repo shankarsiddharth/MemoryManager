b41ea0c65d2a84d905205b5d64a7ec1ad87298d5
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2021-12-13 18:14:45 -0700

    Check bounds in GetFirstSetBit & GetFirstClearBit

--- FinalExam/BitArray/BitArray.cpp
+++ FinalExam/BitArray/BitArray.cpp
@@ -1,8 +1,12 @@
 #include "BitArray.h"
 
+#define DEBUG_BIT_ARRAY_BITS
+#ifdef DEBUG_BIT_ARRAY_BITS
+#include <cstdio>
+#endif
+
 #include <cassert>
 #include <cstring>
-#include <cstdio>
 
 #include <intrin.h>
 #ifdef _WIN64
@@ -11,48 +15,74 @@
 #pragma intrinsic(_BitScanForward, _bittest)
 #endif
 
+#include "../MemoryAlignmentHelper.h"
+
 const size_t BitArray::sBitsPerElement = sizeof(uintptr_t) * 8; //8 bits per byte
 
-BitArray::BitArray(size_t i_numBits, bool i_bInitToZero)
-	: numBits(i_numBits),
-	  arraySize(i_numBits / sBitsPerElement)
+BitArray* BitArray::Create(void* i_pBaseAddress, size_t i_numBits, bool i_bInitToZero)
 {
+	size_t requiredSize = GetRequiredArraySizeForBits(i_numBits);
+	uintptr_t baseAddressOfBitData = reinterpret_cast<uintptr_t>(i_pBaseAddress) - requiredSize;
+	uintptr_t alignedBaseAddressOfBitData = MemoryAlignmentHelper::AlignDown(baseAddressOfBitData);
+
+	uintptr_t rootAddress = alignedBaseAddressOfBitData - sizeof(BitArray);
+
+	BitArray* bitArray = reinterpret_cast<BitArray*>(rootAddress);
+	assert(bitArray);
+
+	//m_pBits = new uintptr_t[i_numBits / sBitsPerElement];
+	return bitArray->Initialize(rootAddress, alignedBaseAddressOfBitData, i_numBits, i_bInitToZero);
+}
+
+//BitArray::~BitArray()
+//{
+//
+//}
+
+void BitArray::Destroy()
+{
+}
+
+BitArray* BitArray::Initialize(uintptr_t i_rootAddress, uintptr_t i_alignedBaseAddressOfBitData, size_t i_numBits, bool i_bInitToZero)
+{
+	pRoot = i_rootAddress;
+	pBaseAddressOfBitData = i_alignedBaseAddressOfBitData;
+	pBitData = reinterpret_cast<uintptr_t*>(i_alignedBaseAddressOfBitData);
+	//sBaseAddressOfBitArray = i_alignedBaseAddressOfBitData;
+
+	numBits = i_numBits;
+	arraySize = i_numBits / sBitsPerElement;
 	const size_t extraBits = i_numBits % sBitsPerElement;
-	if(extraBits != 0)
+	if (extraBits != 0)
 	{
 		arraySize = (i_numBits / sBitsPerElement) + 1;
 	}
-
-	pBitData = new uintptr_t[arraySize];
 
 #ifdef _WIN64
 	const uintptr_t fillValue = (i_bInitToZero ? static_cast<uintptr_t>(0) : UINT64_MAX);
 #else
 	const uintptr_t fillValue = (i_bInitToZero ? static_cast<uintptr_t>(0) : UINT32_MAX);
 #endif
-	
-	//memset(pBitData, i_bInitToZero ? 0 : 1, arraySize);
 
 	for (size_t sizeIndex = 0; sizeIndex < arraySize; sizeIndex++)
 	{
 		pBitData[sizeIndex] = fillValue;
 	}
-}
 
-BitArray::~BitArray()
-{
+	return reinterpret_cast<BitArray*>(pRoot);
 }
 
 void BitArray::Display()
 {
+#ifdef DEBUG_BIT_ARRAY_BITS
 	printf_s("pBits: ");
-	for(size_t sizeIndex = 0; sizeIndex < arraySize; sizeIndex++)
+	for (size_t sizeIndex = 0; sizeIndex < arraySize; sizeIndex++)
 	{
 		uintptr_t Bits = pBitData[sizeIndex];
 		//for(size_t bitIndex = 0; bitIndex < sBitsPerElement; bitIndex++)
 		for (size_t bitIndex = (sBitsPerElement - 1); true; bitIndex--)
 		{
-			if(Bits & (static_cast<uintptr_t>(1) << bitIndex))
+			if (Bits & (static_cast<uintptr_t>(1) << bitIndex))
 			{
 				printf_s("1");
 			}
@@ -60,7 +90,7 @@ void BitArray::Display()
 			{
 				printf_s("0");
 			}
-			if(bitIndex == 0)
+			if (bitIndex == 0)
 			{
 				break;
 			}
@@ -68,6 +98,7 @@ void BitArray::Display()
 		printf_s(" ");
 	}
 	printf_s("\n");
+#endif
 }
 
 void BitArray::InvertAllBits()
@@ -82,6 +113,11 @@ size_t BitArray::GetRequiredArraySizeForBits(size_t i_numBits)
 {
 	const size_t requiredArraySize = (i_numBits / sBitsPerElement);
 	return requiredArraySize;
+}
+
+size_t BitArray::GetBitArraySize() const
+{
+	return numBits;
 }
 
 void BitArray::ClearAll()
@@ -111,7 +147,7 @@ bool BitArray::AreAllBitsClear() const
 	const uintptr_t fillValueToTest = static_cast<uintptr_t>(0);
 	for (size_t sizeIndex = 0; sizeIndex < arraySize; sizeIndex++)
 	{
-		if(pBitData[sizeIndex] != fillValueToTest)
+		if (pBitData[sizeIndex] != fillValueToTest)
 		{
 			return false;
 		}
@@ -167,16 +203,20 @@ bool BitArray::GetFirstClearBit(size_t& o_firstClearBitIndex) const
 	unsigned long Index;
 	for (size_t sizeIndex = 0; sizeIndex < arraySize; sizeIndex++)
 	{
-		const uintptr_t BitsToTest = ~(pBitData[sizeIndex]);
+		const uintptr_t bitsToTest = ~(pBitData[sizeIndex]);
 #ifdef _WIN64
-		const unsigned char isNonZero = _BitScanForward64(&Index, BitsToTest);
+		const unsigned char isNonZero = _BitScanForward64(&Index, bitsToTest);
 #else
-		const unsigned char isNonZero = _BitScanForward(&Index, BitsToTest);
+		const unsigned char isNonZero = _BitScanForward(&Index, bitsToTest);
 #endif
 		if (isNonZero)
 		{
-			o_firstClearBitIndex = (sizeIndex * sBitsPerElement) + Index;
-			return true;
+			const size_t bitIndex = (sizeIndex * sBitsPerElement) + Index;
+			if(bitIndex < numBits)
+			{
+				o_firstClearBitIndex = bitIndex;
+				return true;
+			}
 		}
 	}
 	return false;
@@ -188,14 +228,18 @@ bool BitArray::GetFirstSetBit(size_t& o_firstSetBitIndex) const
 	for (size_t sizeIndex = 0; sizeIndex < arraySize; sizeIndex++)
 	{
 #ifdef _WIN64
-		unsigned char isNonZero = _BitScanForward64(&Index, pBitData[sizeIndex]);
+		const unsigned char isNonZero = _BitScanForward64(&Index, pBitData[sizeIndex]);
 #else
-		unsigned char isNonZero = _BitScanForward(&Index, pBitData[sizeIndex]);
+		const unsigned char isNonZero = _BitScanForward(&Index, pBitData[sizeIndex]);
 #endif
 		if (isNonZero)
 		{
-			o_firstSetBitIndex = (sizeIndex * sBitsPerElement) + Index;
-			return true;
+			const size_t bitIndex = (sizeIndex * sBitsPerElement) + Index;
+			if (bitIndex < numBits)
+			{
+				o_firstSetBitIndex = bitIndex;
+				return true;
+			}
 		}
 	}
 	return false;
@@ -206,14 +250,14 @@ bool BitArray::operator[](size_t i_index) const
 	assert(i_index < numBits);
 #ifdef _WIN64
 	const __int64* MemoryBits = reinterpret_cast<__int64*>(pBitData);
-	const size_t longArrayIndex = i_index / sBitsPerElement;
-	const __int64 longArrayBitIndex = static_cast<__int64>(i_index) % sBitsPerElement;
-	const unsigned char isBitValueEqualToOne = _bittest64(&MemoryBits[longArrayIndex], longArrayBitIndex);
+	const size_t int64ArrayIndex = i_index / sBitsPerElement;
+	const __int64 int64ArrayBitIndex = static_cast<__int64>(i_index) % sBitsPerElement;
+	const unsigned char isBitValueEqualToOne = _bittest64(&MemoryBits[int64ArrayIndex], int64ArrayBitIndex);
 #else
 	const long* MemoryBits = reinterpret_cast<long*>(pBitData);
 	const size_t longArrayIndex = i_index / sBitsPerElement;
 	const long longArrayBitIndex = static_cast<long>(i_index) % sBitsPerElement;
-	const unsigned char isBitOne = _bittest(&MemoryBits[longArrayIndex], longArrayBitIndex);
+	const unsigned char isBitValueEqualToOne = _bittest(&MemoryBits[longArrayIndex], longArrayBitIndex);
 #endif
 	if (isBitValueEqualToOne)
 	{
--- FinalExam/BitArray/BitArray.h
+++ FinalExam/BitArray/BitArray.h
@@ -1,17 +1,16 @@
 #pragma once
-
 #include <cstdint>
 
 class BitArray
 {
 public:
-	BitArray(size_t i_numBits, bool i_bInitToZero = true);
-	~BitArray();
+	static BitArray* Create(void* i_pBaseAddress, size_t i_numBits, bool i_bInitToZero = true);
+	//~BitArray();
+	void Destroy();
 
 	static size_t GetRequiredArraySizeForBits(size_t i_numBits);
 
-	void ClearAll();
-	void SetAll();
+	size_t GetBitArraySize() const;
 
 	bool AreAllBitsClear() const;
 	bool AreAllBitsSet() const;
@@ -19,23 +18,31 @@ public:
 	bool IsBitSet(size_t i_bitNumber) const;
 	bool IsBitClear(size_t i_bitNumber) const;
 
-	void SetBit(size_t i_bitNumber);
-	void ClearBit(size_t i_bitNumber);
 
 	bool GetFirstClearBit(size_t& o_firstClearBitIndex) const;
 	bool GetFirstSetBit(size_t& o_firstSetBitIndex) const;
 
 	bool operator[](size_t i_index) const;
 
+	void ClearAll();
+	void SetAll();
+
+	void SetBit(size_t i_bitNumber);
+	void ClearBit(size_t i_bitNumber);
+	
 	void Display();
 	void InvertAllBits();
 
 	static const size_t sBitsPerElement;
 
 private:
+	//static uintptr_t sBaseAddressOfBitArray;
+
+	BitArray* Initialize(uintptr_t i_rootAddress, uintptr_t i_alignedBaseAddressOfBitData, size_t i_numBits, bool i_bInitToZero = true);
+
+	uintptr_t pRoot = 0;
 	size_t numBits = 0;
 	size_t arraySize = 0;
-
+	uintptr_t pBaseAddressOfBitData = 0;
 	uintptr_t* pBitData = nullptr;
 };
-
