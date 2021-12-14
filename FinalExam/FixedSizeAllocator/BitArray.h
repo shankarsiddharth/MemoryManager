956772939ecabcc61f4bea138d77a27c81d96c68
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2021-12-14 02:02:09 -0700

    Update BitArray

--- FinalExam/FixedSizeAllocator/BitArray.h
+++ FinalExam/FixedSizeAllocator/BitArray.h
@@ -4,28 +4,38 @@
 class BitArray
 {
 public:
-	static BitArray* Create(void* i_pBaseAddress, size_t i_numBits, bool i_bInitToZero = true);
-	//~BitArray();
+	static BitArray* Create(void* i_pBaseAddressOfAvailableMemory, size_t i_sizeOfAvailableMemory, size_t i_numBits, bool i_bInitToZero = true);
 	void Destroy();
 
-	static size_t GetRequiredArraySizeForBits(size_t i_numBits);
+	/*BitArray(size_t i_numBits, bool i_bInitToZero = true);
+	~BitArray();*/
 
-	void ClearAll();
-	void SetAll();
+	static size_t GetRequiredArraySizeForBits(size_t i_numBits);
+	static size_t GetRequiredSizeForObject(void* i_pBaseAddressOfAvailableMemory, size_t i_sizeOfAvailableMemory, size_t i_numBits);
+
+
+	size_t GetBitArraySize() const;
 
 	bool AreAllBitsClear() const;
 	bool AreAllBitsSet() const;
 
-	inline bool IsBitSet(size_t i_bitNumber) const;
-	inline bool IsBitClear(size_t i_bitNumber) const;
+	bool IsBitSet(size_t i_bitNumber) const;
+	bool IsBitClear(size_t i_bitNumber) const;
 
-	void SetBit(size_t i_bitNumber);
-	void ClearBit(size_t i_bitNumber);
 
 	bool GetFirstClearBit(size_t& o_firstClearBitIndex) const;
 	bool GetFirstSetBit(size_t& o_firstSetBitIndex) const;
 
 	bool operator[](size_t i_index) const;
+
+	void ClearAll();
+	void SetAll();
+
+	void SetBit(size_t i_bitNumber);
+	void ClearBit(size_t i_bitNumber);
+
+	void Display();
+	void InvertAllBits();
 
 	static const size_t sBitsPerElement;
 
@@ -36,7 +46,7 @@ private:
 
 	uintptr_t pRoot = 0;
 	size_t numBits = 0;
+	size_t arraySize = 0;
 	uintptr_t pBaseAddressOfBitData = 0;
 	uintptr_t* pBitData = nullptr;
 };
-
