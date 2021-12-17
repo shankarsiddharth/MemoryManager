f70f3714a3ae4096878febd20e489bbc0f334bed
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2021-12-16 22:18:58 -0700

    Code cleanup

--- FinalExam/MemoryBlock.h
+++ FinalExam/MemoryBlock.h
@@ -3,24 +3,9 @@
 
 struct MemoryBlock
 {
-	//void* pBaseAddress;
 	uintptr_t pBaseAddress;
 	size_t	BlockSize;	
 	MemoryBlock* pPreviousBlock;
 	MemoryBlock* pNextBlock;
 	bool bIsAllocated;
-
-	/*MemoryBlock()
-	: pBaseAddress(0),
-	BlockSize(0),
-	pNextBlock(nullptr),
-	pPreviousBlock(nullptr),
-	bIsAllocated(false)
-	{}
-
-	::MemoryBlock& operator=(void* p_memory)
-	{
-		pBaseAddress = static_cast<uint8_t*>(p_memory);
-		return *this;
-	}*/
 };
